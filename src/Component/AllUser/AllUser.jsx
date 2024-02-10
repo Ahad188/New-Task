41448d4dc9046ae59635d04250ab1c82b0a420f1
AhaD <abuahad35188@gmail.com>
2024-02-10 16:52:33 +0600

    search option added card page

--- src/Component/AllUser/AllUser.jsx
+++ src/Component/AllUser/AllUser.jsx
@@ -1,25 +1,57 @@
 import { useEffect, useState } from "react";
 import Card from "../Card/Card";
-import "./alluser.css";
+ 
 const AllUser = () => {
   const [users, setUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+ 
   useEffect(() => {
     fetch("https://dummyjson.com/users")
       .then((res) => res.json())
-      .then((data) => setUsers(data.users));
+      .then((data) => {
+          setUsers(data.users)
+         
+     });
   }, []);
   console.log(users);
+//   
+const handleSearchChange = (event) => {
+     setSearchQuery(event.target.value);
+   };
+ 
+   const filteredUsers = users.filter(
+     (user) =>
+       user.firstName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+       user.lastName.toLowerCase().includes(searchQuery.toLowerCase())
+   );
+
+   
+
 
   return (
     <>
       <h2 className="text-center text-3xl my-3 font-bold">There are All user </h2>
+       
+       {/*  */}
+       <div className="mx-auto mb-3 w-[400px]">
+        <input
+          type="text"
+          placeholder="Search by First Name or Last Name"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="border border-gray-300 rounded-md px-3 py-2 w-full"
+        />
+      </div>
+       {/*  */}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mx-3">
-        {users && users.length > 0 ? (
-          users.map((user) => <Card key={user.id} user={user}></Card>)
-        ) : (
+          {filteredUsers.length > 0 ? (
+          filteredUsers.map((user) => <Card key={user.id} user={user} />)
+               ) : (
           <p>No users found.</p>
-        )}
+          )}
+
+
       </div>
     </>
   );
--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -1,34 +1,41 @@
- 
-import { Link, useNavigate,   } from 'react-router-dom'
-import './cars.css'
-const Card = ({user}) => {
-     const navigate = useNavigate();
-  return (
-    
-     
-     <div className="boxs">
-          <ul className="card-box">
-               <div className="d-img">
-                     <img src={user?.image} className='w-[200px] mx-auto' alt="" />
-               </div>
-               <div className="flex justify-evenly my-3">
-               <li title='First Name' className='name'>Name: {user?.firstName} <span title='Last Name'>{user?.lastName}</span></li>
-               <li title='Email' className='name'>Email : {user?.email}</li>
-               </div>
-               {/* <div className="flex mx-auto justify-between my-3"> */}
-               <li title='Address' className='name px-5 my-4'>Address: {user?.address.address}</li>
-               <li title='Company' className='name px-5 my-4'>Company: {user?.company.name}</li>
-               {/* </div> */}
-          </ul>
-          <div className="overlay-box">
-               <button className="add-cart" onClick={()=>navigate(`/single-dt/${user?.id}`)}>     
-                    
-                     <Link to= '/single-dt'> Details</Link>
-               </button>
-          </div>
-     </div>
-    
-  )
-}
+import { Link, useNavigate } from "react-router-dom";
+import "./cars.css";
+import { useState } from "react";
+const Card = ({ user }) => {
+  const navigate = useNavigate();
 
-export default Card
+  return (
+    <div>
+      <div className="boxs">
+        <ul className="card-box">
+          <div className="d-img">
+            <img src={user?.image} className="w-[200px] mx-auto" alt="" />
+          </div>
+          <div className="flex justify-evenly my-3">
+            <li title="First Name" className="name">
+              Name: {user?.firstName} <span title="Last Name">{user?.lastName}</span>
+            </li>
+            <li title="Email" className="name">
+              Email : {user?.email}
+            </li>
+          </div>
+          {/* <div className="flex mx-auto justify-between my-3"> */}
+          <li title="Address" className="name px-5 my-4">
+            Address: {user?.address.address}
+          </li>
+          <li title="Company" className="name px-5 my-4">
+            Company: {user?.company.name}
+          </li>
+          {/* </div> */}
+        </ul>
+        <div className="overlay-box">
+          <button className="add-cart" onClick={() => navigate(`/single-dt/${user?.id}`)}>
+            <Link to="/single-dt"> Details</Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
